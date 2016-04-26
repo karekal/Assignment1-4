3a9575fb1cb061384f77360e8791fbcae832dfa3
srima karekal <srima589.karekal@gmail.com>
2016-04-26 22:19:32 +0530

    commit

---
+++ src/Assignment1_4.java
@@ -0,0 +1,13 @@
+
+public class Assignment1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x=5,y=10;
+		 System.out.println("x + y * 2="+(x + y * 2));
+		 System.out.println("x - y + 2="+(x - y + 2));
+		 System.out.println("(x+y) * 2="+((x+y) * 2));
+		 System.out.println("y % x="+(y % x));
+	}
+
+}
